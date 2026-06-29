dcd20b6eb1fff32b1fb282e3661f2b6795b49f20
nisitanisin2-ctrl <nisitanisin2@gmail.com>
2026-06-29 17:01:58 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'excalc-v8';
+const CACHE = 'excalc-v23';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
